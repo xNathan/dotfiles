1a723a533651bba329a25a0d41a09d6aacc821f0
xNathan <i@denghw.cn>
2019-08-15 00:23:53 +0800

    Add proxy alias

--- dotfiles/zshrc.d/aliases.zsh
+++ dotfiles/zshrc.d/aliases.zsh
@@ -1,3 +1,5 @@
 # add alias
 
 alias pc='proxychains4'
+alias setproxy='export https_proxy=http://127.0.0.1:7890;export http_proxy=http://127.0.0.1:7890;export all_proxy=socks5h://127.0.0.1:7891'
+alias unsetproxy='unset https_proxy; unset http_proxy; unset all_proxy;'
