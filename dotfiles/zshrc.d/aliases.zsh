7443764636b94ca655f0fad782bca99c61b62641
xNathan <i@denghw.cn>
2019-08-15 15:34:59 +0800

    Add tmux alias

--- dotfiles/zshrc.d/aliases.zsh
+++ dotfiles/zshrc.d/aliases.zsh
@@ -3,3 +3,11 @@
 alias pc='proxychains4'
 alias setproxy='export https_proxy=http://127.0.0.1:7890;export http_proxy=http://127.0.0.1:7890;export all_proxy=socks5h://127.0.0.1:7891'
 alias unsetproxy='unset https_proxy; unset http_proxy; unset all_proxy;'
+
+# tmux alias, copied from oh-my-zsh
+alias ta='tmux attach -t'
+alias tad='tmux attach -d -t'
+alias ts='tmux new-session -s'
+alias tl='tmux list-sessions'
+alias tksv='tmux kill-server'
+alias tkss='tmux kill-session -t'
