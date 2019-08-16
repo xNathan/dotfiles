2e114bea6a5f7b050b71483c29a577a42ac16a56
xNathan <i@denghw.cn>
2019-08-16 14:18:46 +0800

    Add gpg ssh agent config

--- dotfiles/zshrc.d/exports.zsh
+++ dotfiles/zshrc.d/exports.zsh
@@ -1,1 +1,8 @@
 # add export
+
+export GPG_TTY=$(tty)
+
+unset SSH_AGENT_PID
+if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
+  export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
+fi
