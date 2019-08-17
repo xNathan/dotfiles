197378cc5faf9cadf35b59b443a3e22da3c339a1
xNathan <i@denghw.cn>
2019-08-17 13:14:51 +0800

    feat(zshrc): add powerlevel10k theme config

--- dotfiles/zshrc.d/exports.zsh
+++ dotfiles/zshrc.d/exports.zsh
@@ -1,5 +1,7 @@
 # add export
 
+[ -f ~/.p10k.zsh ] && source ~/.p10k.zsh
+
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 export FZF_DEFAULT_COMMAND='fd --type f'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
