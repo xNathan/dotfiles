05f1abe7439330ea83cd77ab0aa5a3be919b3c81
xNathan <i@denghw.cn>
2019-08-17 13:01:12 +0800

    reafctor(zshrc): move FZF config to exports.zsh

--- dotfiles/zshrc.d/exports.zsh
+++ dotfiles/zshrc.d/exports.zsh
@@ -1,5 +1,9 @@
 # add export
 
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+export FZF_DEFAULT_COMMAND='fd --type f'
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+
 export GPG_TTY=$(tty)
 
 unset SSH_AGENT_PID
