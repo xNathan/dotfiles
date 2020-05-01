26e98d895a82f0eb9780c11b41cb2746f0ceccbb
xNathan <me@xnathan.com>
2020-05-01 12:34:25 +0800

    fix(zshrc.d): use double brackets

--- dotfiles/zshrc.d/exports.zsh
+++ dotfiles/zshrc.d/exports.zsh
@@ -1,8 +1,8 @@
 # add export
 
-[ -f ~/.p10k.zsh ] && source ~/.p10k.zsh
+[[ -f ~/.p10k.zsh ]] && source ~/.p10k.zsh
 
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+[[ -f ~/.fzf.zsh ]] && source ~/.fzf.zsh
 export FZF_DEFAULT_COMMAND='fd --type f'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
