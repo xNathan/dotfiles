396d934d57baa50e76e4ad80e635995334c3a9f4
xNathan <me@xnathan.com>
2023-07-26 10:03:59 +0800

    add macos defaults

---
+++ home/.chezmoiscripts/darwin/run_once_after_defaults.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# Set a blazingly fast keyboard repeat rate
+defaults write NSGlobalDomain KeyRepeat -int 1
+defaults write NSGlobalDomain InitialKeyRepeat -int 15
+
+# Trackpad: enable tap to click for this user and for the login screen
+defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+
+#Enable key repeat in VSCodeVim
+defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
