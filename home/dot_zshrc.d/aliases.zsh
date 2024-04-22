d8ba52c5ab54cf52935049a4ac6b2cbdb49aba95
xNathan <xNathan@users.noreply.github.com>
2024-04-22 17:47:23 +0800

    update zsh config

--- home/dot_zshrc.d/aliases.zsh
+++ home/dot_zshrc.d/aliases.zsh
@@ -2,18 +2,7 @@ alias pc='proxychains4'
 alias setproxy='export https_proxy=http://127.0.0.1:7890;export http_proxy=http://127.0.0.1:7890;export all_proxy=socks5h://127.0.0.1:7891'
 alias unsetproxy='unset https_proxy; unset http_proxy; unset all_proxy;'
 
-# tmux alias, copied from oh-my-zsh
-alias ta='tmux attach -t'
-alias tad='tmux attach -d -t'
-alias ts='tmux new-session -s'
-alias tl='tmux list-sessions'
-alias tksv='tmux kill-server'
-alias tkss='tmux kill-session -t'
-
-
-alias dotdrop="dotdrop --cfg=$HOME/.dotfiles/config.yaml"
-alias dotgit="git -C $HOME/.dotfiles"
-alias dotsync="dotgit pull && dotgit add -A && dotgit commit && dotgit push; dotdrop install"
+alias ssh="TERM=xterm-256color $(which ssh)"
 
 if [[ "$(command -v nvim)" ]]; then
     alias v='nvim'
