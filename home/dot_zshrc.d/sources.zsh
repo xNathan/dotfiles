8a3799ce32589ac752e734cc04807f4bae37e036
xNathan <me@xnathan.com>
2024-04-25 18:30:55 +0800

    move mise/direnv to omz plugin

--- home/dot_zshrc.d/sources.zsh
+++ home/dot_zshrc.d/sources.zsh
@@ -1,5 +1,3 @@
 [[ -f ~/.p10k.zsh ]] && source ~/.p10k.zsh
 [[ -f ~/.fzf.zsh ]] && source ~/.fzf.zsh
 
-[[ "$(command -v mise)" ]] && eval "$(mise activate zsh)"
-[[ "$(command -v direnv)" ]] && eval "$(direnv hook zsh)"
